cf149deb5d5b96cae604fc0fced6a105f5c16305
luizdopc <luizotaviosoares@outlook.com>
2020-01-06 18:11:43 -0300

    initial commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const { GraphQLServer } = require('graphql-yoga');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const resolvers = require('./resolvers');
+
+mongoose.connect('mongodb://0.0.0.0:27017/graphqlnode', {
+  useNewUrlParser: true
+});
+
+const server = new GraphQLServer({
+  typeDefs: path.resolve(__dirname, 'schema.graphql'),
+  resolvers
+});
+
+server.start();
